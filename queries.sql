cfe1054783462102418b6bdd3e77bd2297c5cfa4
mohammedanaz <mohammedanaz@gmail.com>
2024-07-31 11:34:54 +0530

    add

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 -- select * from students;
 
-create table aaa
-id bigint
-name varchar(100) not null
-add constraint my_pk primary key (id);
+create table aaa(
+id bigint,
+name varchar(100) not null,
+constraint my_pk primary key (id)
+);
