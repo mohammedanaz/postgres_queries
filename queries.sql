ca1566b3788b9abfedf6a0b4efad3e5adf5510e1
mohammedanaz <mohammedanaz@gmail.com>
2024-07-31 18:02:25 +0530

    aaa

--- queries.sql
+++ queries.sql
@@ -1,10 +1,4 @@
 -- select * from students;
 -- select * from marks;
 
-select name, 
-case
-when total_marks > 170 then 'High'
-when total_marks between 160 and 170 then 'Normal'
-else 'Low'
-end as status
-from marks;
+select length('string');
