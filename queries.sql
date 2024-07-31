3c1a2377a21c8ce4defaed1a3b7799ba961c0d66
mohammedanaz <mohammedanaz@gmail.com>
2024-07-31 12:34:45 +0530

    aaa

--- queries.sql
+++ queries.sql
@@ -3,8 +3,8 @@
 
 select name, 
 case
-when total_marks > 170 then 'High',
-when total_marks between 160 and 170 then 'Normal',
-else 'Low',
+when total_marks > 170 then 'High'
+when total_marks between 160 and 170 then 'Normal'
+else 'Low'
 end as status
 from marks;
