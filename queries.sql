1c971274e52536d08940ce6004ab74b93c5bf333
mohammedanaz <mohammedanaz@gmail.com>
2024-07-31 10:34:55 +0530

    next val

--- queries.sql
+++ queries.sql
@@ -1,7 +1,1 @@
-insert into students
-(id, dept_id, name, gender, state, city, concession)
-values (4, 101, 'Ajay', 'Male', 'Maharashtra', 'Mumbai', 5)
-on conflict (id)
-do update set 
-name = excluded.name, 
-gender = excluded.gender;
+select currentval('my_temp_seq');
