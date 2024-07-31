ef82bef8cac9a09d57b049bf7d7c72f576f9cb33
mohammedanaz <mohammedanaz@gmail.com>
2024-07-31 10:23:11 +0530

    updert

--- queries.sql
+++ queries.sql
@@ -1,2 +1,7 @@
-select english, sum(total_marks) , count(*)
-from marks;
+insert into students
+(id, dept_id, name, gender, state, city, concession)
+values (4, 101, 'Ajay', 'Male', 'Maharashtra', 'Mumbai', 5)
+on conflict (id)
+do update set 
+name = excluded.name, 
+gender = excluded.gender;
