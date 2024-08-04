a4bafb9d487991a2eb64c6bc26fbfa45139feb16
mohammedanaz <mohammedanaz@gmail.com>
2024-08-04 09:24:47 +0530

    sum salary

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 -- select * from students;
 -- select * from marks;
 
-select count(*), round(avg(salary), 2)
-from worker;
+select sum(salary) 
+from worker
+group by department_id
+having department_id = 1;
