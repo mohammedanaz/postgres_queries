5ea029570235684fd5acd8f1eca0efde1bac4c93
mohammedanaz <mohammedanaz@gmail.com>
2024-08-04 10:02:04 +0530

    explain analyze

--- queries.sql
+++ queries.sql
@@ -1,8 +1,12 @@
 -- select * from students;
 -- select * from marks;
+-- explain analyze
+-- select * from worker;
 
 select *
 from worker w
+inner join department d
+on (w.department_id = d.id)
 where salary = (
     select max(salary)
     from worker sub_w
